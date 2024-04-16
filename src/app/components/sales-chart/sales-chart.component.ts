08c0556ca077a5ea44521f4d5fb001ad724eec11
Mikhail Batsian <batsianm@yahoo.com>
2024-04-16 17:07:12 +0300

    Added header and footer, added date interval picker

--- src/app/components/sales-chart/sales-chart.component.ts
+++ src/app/components/sales-chart/sales-chart.component.ts
@@ -2,22 +2,36 @@ import { Component, ViewChild} from '@angular/core';
 import { CommonModule , DatePipe } from '@angular/common';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { ChartComponent } from '../chart/chart.component';
 import { SaleService } from '../../services/sale.service';
 
 @Component({
   selector: 'app-sales-chart',
   standalone: true,
-  imports: [CommonModule, ChartComponent, MatButton, MatMiniFabButton, MatIcon],
-  providers: [DatePipe],
+  imports: [CommonModule, ChartComponent, MatButton, MatMiniFabButton, MatIcon, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatSelectModule],
+  providers: [DatePipe, provideNativeDateAdapter()],
   templateUrl: './sales-chart.component.html',
   styleUrl: './sales-chart.component.css'
 })
 
 export class SalesChartComponent {
   private skipElementsCounter: number = 0
-  private takeElements: number = 15;
+  private takeElementsCount: number = 15;
+  
+  dateIntervals: any[] = [
+    {id: 'quarter', viewValue: 'Quarter'},
+    {id: 'month', viewValue: 'Month'},
+    {id: 'week', viewValue: "Week"},
+    {id: 'dayofyear', viewValue: 'Day'}
+  ];
 
+  selectedDateInterval: string = 'quarter';
   shouldHideBtnPrevious: boolean = true;
   shouldHideBtnNext: boolean = false;
 
@@ -27,37 +41,63 @@ export class SalesChartComponent {
   @ViewChild(ChartComponent) chart!:ChartComponent;
 
   constructor(private service: SaleService,  private datePipe: DatePipe) {
-    this.fetchSalesData();
+    const filter = this.getFilter();
+    this.fetchSalesData(filter);
+  }
+
+  onChangeDateInterval(value: any){
+    this.resetElementsCounter();
+
+    const filter = this.getFilter();
+    filter.timeInterval = value;
+    this.fetchSalesData(filter);
+  }
+
+  onChangeStartDate(){
+
+  }
+  
+  onChangeEndDate(){
+
   }
 
   resetZoom(){
     this.chart.ResetZoom();
   }
 
+  resetElementsCounter(){
+    this.skipElementsCounter = 0;
+  }
+
   getNexData(){
     this.resetZoom();
-    this.skipElementsCounter = this.skipElementsCounter + this.takeElements;
-    this.fetchSalesData();
+    this.skipElementsCounter = this.skipElementsCounter + this.takeElementsCount;
+    const filter = this.getFilter();
+    this.fetchSalesData(filter);
   }
 
   getPreviousData(){
     this.resetZoom();
     //todo: in future version refactor to get saledata from cache
-    this.skipElementsCounter = this.skipElementsCounter - this.takeElements;
-    this.fetchSalesData();
+    this.skipElementsCounter = this.skipElementsCounter - this.takeElementsCount;
+    const filter = this.getFilter();
+    this.fetchSalesData(filter);
   }
 
-  fetchSalesData(){
+  getFilter(){
     let filter = {
       skip: this.skipElementsCounter,
-      take: this.takeElements,
-      timeInterval: "week",
+      take: this.takeElementsCount,
+      timeInterval: this.selectedDateInterval,
       startDate: "2020-01-02 00:00:00.0000000",
       endDate: "2022-03-23 00:00:00.0000000"
     };
 
+    return filter;
+  }
+
+  fetchSalesData(filter: any){
     this.service.GetSalesAmounts(filter).subscribe(response => {
-     
       if(!response.length) {
         this.shouldHideBtnNext = true;
         alert("No any data");
@@ -65,7 +105,7 @@ export class SalesChartComponent {
       }
 
       this.shouldHideBtnPrevious = !this.skipElementsCounter;
-      this.shouldHideBtnNext = false;
+      this.shouldHideBtnNext = response.length < this.takeElementsCount;
 
       this.startDates = response.map(x => this.datePipe.transform(x.periodStartDate, 'MM/dd/yyyy')!);
       this.salesAmounts = response.map(x => x.salesAmount);
